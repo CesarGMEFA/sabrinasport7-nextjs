7ea02299738fdc3e7ab88e119d77c4e322b5bcf4
César Mejias <cesarfar376@gmail.com>
2023-12-26 10:47:07 -0400

    Update Pagination component styles

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -107,7 +107,7 @@ function PaginationNumber({
   isActive: boolean;
 }) {
   const className = clsx(
-    "flex h-10 w-10 items-center justify-center text-sm border",
+    "flex w-7 h-7 min-[430px]:h-10 min-[430px]:w-10 items-center justify-center text-sm border",
     {
       "rounded-l-md": position === "first" || position === "single",
       "rounded-r-md": position === "last" || position === "single",
@@ -136,7 +136,7 @@ function PaginationArrow({
   isDisabled?: boolean;
 }) {
   const className = clsx(
-    "flex h-10 w-10 items-center justify-center rounded-md border",
+    "flex w-7 h-7 min-[430px]:h-10 min-[430px]:w-10 items-center justify-center rounded-md border",
     {
       "pointer-events-none text-gray-300": isDisabled,
       "hover:bg-gray-100": !isDisabled,
