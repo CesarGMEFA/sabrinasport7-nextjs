2efe3f4154ef5b07474446967e2518950a7928d5
César Mejias <cesarfar376@gmail.com>
2024-01-06 16:33:44 -0400

    Add discount percentage to ProductCard component

--- src/components/Product/ProductCard/index.tsx
+++ src/components/Product/ProductCard/index.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/card";
 import { Button } from "../../ui/button";
 
-import { ShoppingCart } from "lucide-react";
+import { Percent, ShoppingCart } from "lucide-react";
 
 import { Product } from "@/lib/interfaces/Product.interface";
 
@@ -25,20 +25,27 @@ export default function ProductCard({ data }: Props) {
 
   const prices = price_html.match(regex);
   const price = prices?.[0] || "";
-  const sale_price = prices?.[1] || undefined;
+  const sale_price = prices?.[1] || "";
+
+  let discount: number | string = 0;
+  if (sale_price) {
+    const p = Number(price.replace(",", "."));
+    const s = Number(sale_price.replace(",", "."));
+    discount = Math.floor(((p - s) / p) * 100);
+    discount.toString()
+  }
 
   return (
-    <Card className="w-[92%] h-min sm:w-64 md:w-60 lg:w-56 p-3 mx-auto shadow-md">
+    <Card className="w-[92%] h-min sm:w-64 md:w-60 lg:w-56 p-3 mx-auto shadow-md relative">
+      {sale_price && (
+        <div className="flex items-center bg-red-600 text-white font-bold py-1 px-2 rounded-tl-lg rounded-br-lg absolute top-0 left-0">
+          {discount}
+          <span className="ml-0.5">%</span>
+        </div>
+      )}
       <CardHeader className="p-0">
         <Link href={`/productos/${data?.id}`}>
           <div className="w-full h-40 min-[480px]:h-60 md:h-56 lg:h-52 flex justify-center items-center">
-            {/* <Image
-              src={data.images[0]?.src}
-              alt={data.name}
-              width={216}
-              height={260}
-              className="w-full h-full object-contain"
-            /> */}
             <Image
               src={data?.images[0]?.src || "/images/placeholder.png"}
               alt={data?.name || "Imagen del producto"}
