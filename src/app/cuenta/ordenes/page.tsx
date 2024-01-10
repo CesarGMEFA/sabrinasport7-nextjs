de948a509ea9ad3f7ab546927477ab2d0294d774
César Mejias <cesarfar376@gmail.com>
2024-01-10 17:09:47 -0400

    poner en comentario asuntos del estado en la pagina order

--- src/app/cuenta/ordenes/page.tsx
+++ src/app/cuenta/ordenes/page.tsx
@@ -3,10 +3,19 @@ import React from "react";
 import { Separator } from "@/components/ui/separator";
 import { OrderCard } from "@/components/Orders/OrderCard";
 import { useAuth } from "@/lib/hook/useAuth";
+// import { UserStore } from "@/lib/interfaces/UserStore.interface";
+// import { useUserStore } from "@/lib/store/useUserStore";
+// import { useUser } from "@/lib/store/user";
 
 type Props = {};
 
 function Orders({}: Props) {
+  // const userStore = useUserStore<UserStore, UserStore>(
+  //   useUser,
+  //   (state: any) => state
+  // );
+
+  // const { user } = userStore || {};
 
   useAuth()
 
